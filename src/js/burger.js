d54bed158506b807186a04c215ae4e2a7f36a78b
Antooon32 <robotkartoshka@gmail.com>
2025-11-27 15:04:08 +0200

    fix error burger.js

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,10 +1,17 @@
 function burgerMenu() {
   const openBtn = document.querySelector('[data-burger-open]');
-  const burger = document.querySelector('[data-burger]');
+  const burger = document.querySelector('[data-burger]'); 
   const closeBtn = document.querySelector('[data-burger-close]');
   const backdrop = document.querySelector('[data-backdrop]');
 
   if (!openBtn || !burger || !closeBtn || !backdrop) return;
+
+  const MOBILE_WIDTH = 767;
+  function initializeBurgerState() {
+    if (window.innerWidth < MOBILE_WIDTH) {
+      burger.classList.add('is-open');
+    }
+  }
 
   function toggleBurger() {
     burger.classList.toggle('is-open');
@@ -12,6 +19,7 @@ function burgerMenu() {
     document.body.classList.toggle('no-scroll');
   }
 
+  initializeBurgerState();
   openBtn.addEventListener('click', toggleBurger);
   closeBtn.addEventListener('click', toggleBurger);
   backdrop.addEventListener('click', toggleBurger);
