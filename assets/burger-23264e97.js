826fa61387ad2c70c320141f9a839daf88e8e28d
Antooon32 <robotkartoshka@gmail.com>
2025-11-27 11:08:54 +0000

    Deploying to gh-pages from @ Antooon32/britlex-team-project@4977b43fe4905bd637700445eac76566739ad444 🚀

--- assets/burger-23264e97.js
+++
@@ -1,2 +0,0 @@
-//! Burger
-const t=document.querySelector("[data-burger-open]"),r=document.querySelector("[data-burger]"),o=document.querySelector("[data-burger-close]");t.addEventListener("click",e);o.addEventListener("click",e);function e(){r.classList.toggle("displaynone"),document.body.classList.toggle("no-scroll")}
